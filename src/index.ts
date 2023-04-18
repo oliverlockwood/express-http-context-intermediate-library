82a0d391bed83a07bdde2219930661e4badac3be
Oliver Lockwood <olockwood@dalet.com>
2023-04-18 10:14:06 +0100

    Add some explicit typing for requestId

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ function init(app: Application) {
 	app.use(httpContext.middleware);
 
 	app.use((req: Request, _: Response, next: NextFunction) => {
-		const requestId = req.header(REQUEST_ID_HTTP_HEADER_NAME) || nanoid();
+		const requestId: string = req.header(REQUEST_ID_HTTP_HEADER_NAME) || nanoid();
 
-		httpContext.set(REQUEST_ID_CONTEXT_KEY, requestId);
+		httpContext.set<string>(REQUEST_ID_CONTEXT_KEY, requestId);
 		next();
 	});
 }
