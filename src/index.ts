8fdccd6dc8ee6069421516989d997960f306bb1c
Oliver Lockwood <olockwood@dalet.com>
2023-04-18 10:23:34 +0100

    Set the generated request ID as a response header as well as into the HTTP context

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,19 @@ import { nanoid } from 'nanoid';
 const REQUEST_ID_HTTP_HEADER_NAME = 'my-request-id';
 const REQUEST_ID_CONTEXT_KEY = 'myRequestId';
 
+// less realistic, but useful for testing!
+const REQUEST_ID_IN_RESPONSE_HTTP_HEADER_NAME = 'my-request-id-in-response'
 
 function init(app: Application) {
 
 	app.use(httpContext.middleware);
 
-	app.use((req: Request, _: Response, next: NextFunction) => {
+	app.use((req: Request, res: Response, next: NextFunction) => {
 		const requestId: string = req.header(REQUEST_ID_HTTP_HEADER_NAME) || nanoid();
 
 		httpContext.set<string>(REQUEST_ID_CONTEXT_KEY, requestId);
+		res.header(REQUEST_ID_IN_RESPONSE_HTTP_HEADER_NAME, requestId);
+
 		next();
 	});
 }
@@ -21,5 +25,6 @@ function init(app: Application) {
 
 export {
 	init,
-	REQUEST_ID_CONTEXT_KEY
+	REQUEST_ID_CONTEXT_KEY,
+	REQUEST_ID_IN_RESPONSE_HTTP_HEADER_NAME
 }
